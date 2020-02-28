16118fd4c442435f9ebde64367cfe2b0783be3a4
Kingsley <k@n.com>
2020-02-28 11:08:35 +0100

    remove working code and added task comments

--- js/master.js
+++ js/master.js
@@ -10,19 +10,26 @@ function handleAPIResponse(comments) {
     
     // only get comments from post 1
     const filteredComments = comments.filter(function(comment) {
-        return comment.postId === 1;
+        /**
+         * TASK 1
+         * filter out comments that dont have .postId of 1
+         */ 
+        return true;
     });
     // map data to HTML
     const htmlArray = filteredComments.map(function(comment) {
-        return '<article>' +
-                    '<h2>' + comment.name + '</h2>' +
-                    '<a href="emilto:' + comment.email + '">' + comment.email + '</a>' +
-                    '<p>' + comment.body + '</p>' +
-                    '<div>Post ID: ' + comment.postId + '</div>' +
-                '</article>';
+        /**
+        * TASK 2
+        * create HTML from comment object using a tempate literal
+        */ 
+        return ``;
     });
-    // join the HTML into a single string
-    const html = htmlArray.join("");
+    /**
+     * TASK 3
+     * Pass an empty string to join() so that the HTML gets together with no separator
+     * By default join() will separate items with a comma 
+     */ 
+    const html = htmlArray.join();
 
     // set the HTML into the page
     document.getElementById("comments").innerHTML = html;
